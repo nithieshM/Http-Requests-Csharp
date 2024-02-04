d920d57a7631d0f4dc0492b9fc914ff21fc6ce84
Nithiesh Mohan <nithiesh164@gmail.com>
2024-02-04 20:13:39 +0530

    Got JSON data from the api

--- HttpRequestPractice/Program.cs
+++ HttpRequestPractice/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace HttpRequestPractice;
+using System.Net.Http.Headers;
 
 class Program
 {
-    static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+    
+        await ProcessRepositoriesAsync(client);
+    }
+    static async Task ProcessRepositoriesAsync(HttpClient client)
+    {
+        var json = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+        
+        Console.Write(json);
     }
 }
